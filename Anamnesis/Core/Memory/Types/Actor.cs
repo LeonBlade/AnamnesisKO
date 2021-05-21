35770efa1e3cb2e9630c2f61b3eb4822cff495a2
Aina Snow <dream-flower-rabbit@outlook.com>
2021-05-22 01:21:35 +0800

    fix kr name error

--- Anamnesis/Core/Memory/Types/Actor.cs
+++ Anamnesis/Core/Memory/Types/Actor.cs
@@ -8,6 +8,7 @@ namespace Anamnesis.Memory
 	using System.Collections.Generic;
 	using System.Reflection;
 	using System.Runtime.InteropServices;
+	using System.Text;
 	using System.Threading.Tasks;
 	using Anamnesis.Character;
 	using Anamnesis.Character.Utilities;
@@ -25,10 +26,11 @@ namespace Anamnesis.Memory
 	{
 		public const int ObjectKindOffset = 0x008c;
 		public const int RenderModeOffset = 0x0104;
+		public const int TransparencyOffset = 0x0108;
 
 		[FieldOffset(0x0030)]
-		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 30)]
-		public string Name;
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 30)]
+		public byte[] NameBytes;
 
 		[FieldOffset(0x0080)] public int DataId;
 		[FieldOffset(ObjectKindOffset)] public ActorTypes ObjectKind;
@@ -39,12 +41,35 @@ namespace Anamnesis.Memory
 		[FieldOffset(RenderModeOffset)] public RenderModes RenderMode;
 		[FieldOffset(0x01B4)] public int ModelType;
 		[FieldOffset(0x01E2)] public byte ClassJob;
-		[FieldOffset(0x07C4)] [MarshalAs(UnmanagedType.I1)]public bool IsAnimating;
 		[FieldOffset(0x0F08)] public Weapon MainHand;
 		[FieldOffset(0x0F70)] public Weapon OffHand;
 		[FieldOffset(0x1040)] public Equipment Equipment;
-		[FieldOffset(0x180C)] public float Transparency;
+		[FieldOffset(TransparencyOffset)] public float Transparency;
 		[FieldOffset(0x1878)] public Appearance Customize;
+
+		public string Name
+		{
+			get
+			{
+				int i;
+				for (i = 0; i < this.NameBytes.Length; i++)
+				{
+					if (this.NameBytes[i] == 0)
+						break;
+				}
+
+				return Encoding.UTF8.GetString(this.NameBytes, 0, i);
+			}
+
+			set
+			{
+				byte[] bytes = Encoding.UTF8.GetBytes(value);
+				if (bytes.Length >= 30)
+					throw new Exception($"{value} too long");
+				else
+					this.NameBytes = bytes;
+			}
+		}
 
 		public string Id => this.Name + this.DataId;
 	}
@@ -65,18 +90,17 @@ namespace Anamnesis.Memory
 		{
 		}
 
-		[ModelField] public string Name { get; set; } = string.Empty;
+		public string Name { get; set; } = string.Empty;
 		[ModelField] public int DataId { get; set; }
-		[ModelField][Refresh] public ActorTypes ObjectKind { get; set; }
+		[ModelField] [Refresh] public ActorTypes ObjectKind { get; set; }
 		[ModelField] public byte SubKind { get; set; }
-		[ModelField][Refresh] public AppearanceViewModel? Customize { get; set; }
-		[ModelField][Refresh] public int ModelType { get; set; }
-		[ModelField] public bool IsAnimating { get; set; }
-		[ModelField][Refresh] public RenderModes RenderMode { get; set; }
+		[ModelField] [Refresh] public AppearanceViewModel? Customize { get; set; }
+		[ModelField] [Refresh] public int ModelType { get; set; }
+		[ModelField] [Refresh] public RenderModes RenderMode { get; set; }
 		[ModelField] public float Transparency { get; set; }
-		[ModelField][Refresh] public EquipmentViewModel? Equipment { get; set; }
-		[ModelField][Refresh] public WeaponViewModel? MainHand { get; set; }
-		[ModelField][Refresh] public WeaponViewModel? OffHand { get; set; }
+		[ModelField] [Refresh] public EquipmentViewModel? Equipment { get; set; }
+		[ModelField] [Refresh] public WeaponViewModel? MainHand { get; set; }
+		[ModelField] [Refresh] public WeaponViewModel? OffHand { get; set; }
 		[ModelField] public ModelViewModel? ModelObject { get; set; }
 
 		public bool AutomaticRefreshEnabled { get; set; } = true;
